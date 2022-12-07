e3b85d2f7e47ea1e79e786976e69f4424635415f
LuisSilva11 <104436747+LuisSilva11@users.noreply.github.com>
2022-12-06 21:58:22 -0500

    Preguntas completas

--- js/luis.js
+++ js/luis.js
@@ -49,84 +49,52 @@ question3.addEventListener('mouseleave',()=>{
     }
 })
 
-const question4 = document.getElementById('create_');
+const question4 = document.getElementById('ques_4');
 question4.addEventListener('mouseleave',()=>{
-    const valor = document.getElementById('create_').value
+    const valor = document.getElementById('ques_4').value
     if(valor == 1){
         question4.classList.remove('incorrecto');
         question4.classList.add('correcto');
-        validar['pregunta3'] = true
+        validar['pregunta4'] = true
         console.log(validar)
     }else{
         question4.classList.remove('correcto');
         question4.classList.add('incorrecto');
-        validar['pregunta3'] = false
+        validar['pregunta4'] = false
     }
 })
 
-const question5 = document.getElementById('insert_');
+const question5 = document.getElementById('ques_5');
 question5.addEventListener('mouseleave',()=>{
-    const valor = document.getElementById('insert_').value;
-    if(valor== `db.alumnos.insert({nombre:"pedro"})` || valor == `db.alumnos.insert({ nombre: "pedro" })` || valor == `db.alumnos.insert( { nombre: "pedro" } )`){
+    const valor = document.getElementById('ques_5').value;
+    if(valor== "db.producto.find({precio:{$not:{$gt:2000}}})"){
         question5.classList.remove('incorrecto');
         question5.classList.add('correcto');
-        validar['pregunta4'] = true;
+        validar['pregunta5'] = true;
     }else{
         question5.classList.remove('correcto');
         question5.classList.add('incorrecto');
-        validar['pregunta4'] = false;
+        validar['pregunta5'] = false;
     }
 })
 
-const question6 = document.getElementById('update_');
+const question6 = document.getElementById('ques_6');
 question6.addEventListener('mouseleave',()=>{
-    const valor = document.getElementById('update_').value;
-    if(valor== `db.profesores.update({_id:1},{$set:{nombre:"lucas"}})` || valor== `db.profesores.update( {_id:1} , {$set:{nombre:"lucas"} } )`|| valor== `db.profesores.update( {_id:1} , { $set:{nombre:"lucas"} } )` || valor== `db.profesores.update({ _id:1 },{ $set:{ nombre:"lucas" }})`){
+    const valor = document.getElementById('ques_6').value;
+    if(valor== 2){
         question6.classList.remove('incorrecto');
         question6.classList.add('correcto');
-        validar['pregunta5'] = true; 
+        validar['pregunta6'] = true; 
     }else{
         question6.classList.remove('correcto');
         question6.classList.add('incorrecto');
-        validar['pregunta5'] = false; 
-    }
-})
- 
-const question7 = document.getElementById('drop_3');
-question7.addEventListener('mouseleave',()=> {
-    const valor = document.getElementById('drop_3').value;
-    if(valor == 4){
-        question7.classList.remove('incorrecto');
-        question7.classList.add('correcto');
-        validar['pregunta6'] = true; 
-    }else{
-        question7.classList.remove('correcto');
-        question7.classList.add('incorrecto');
         validar['pregunta6'] = false; 
     }
-
-});
-
-const question8 = document.getElementById('drop_4');
-question8.addEventListener('mouseleave',()=> {
-    const valor = document.getElementById('drop_4').value;
-    if(valor == "db.dropDatabase()"){
-        question8.classList.remove('incorrecto');
-        question8.classList.add('correcto');
-        validar['pregunta7'] = true; 
-        console.log(validar)
-    }else{
-        question8.classList.remove('correcto');
-        question8.classList.add('incorrecto');
-        validar['pregunta7'] = false; 
-    }
-
-});
-
+})
 
 question1.addEventListener('blur',()=>{
     const valor = document.getElementById('ques_1').value
-    if(valor==1){
+    if(valor==2){
         question1.classList.remove('incorrecto');
         question1.classList.add('correcto');
         validar['pregunta1'] = true;
@@ -167,78 +135,47 @@ question3.addEventListener('blur',()=>{
 
 
 question4.addEventListener('blur',()=>{
-    const valor = document.getElementById('create_').value
+    const valor = document.getElementById('ques_4').value
     if(valor == 1){
         question4.classList.remove('incorrecto');
         question4.classList.add('correcto');
-        validar['pregunta3'] = true
+        validar['pregunta4'] = true
         console.log(validar)
     }else{
         question4.classList.remove('correcto');
         question4.classList.add('incorrecto');
-        validar['pregunta3'] = false
+        validar['pregunta4'] = false
     }
 })
 
 
 question5.addEventListener('blur',()=>{
-    const valor = document.getElementById('insert_').value;
-    if(valor== `db.alumnos.insert({nombre:"pedro"})` || valor == `db.alumnos.insert({ nombre: "pedro" })` || valor == `db.alumnos.insert( { nombre: "pedro" } )`){
+    const valor = document.getElementById('ques_5').value;
+    if(valor== "db.producto.find({precio:{$not:{$gt:2000}}})"){
         question5.classList.remove('incorrecto');
         question5.classList.add('correcto');
-        validar['pregunta4'] = true;
+        validar['pregunta5'] = true;
     }else{
         question5.classList.remove('correcto');
         question5.classList.add('incorrecto');
-        validar['pregunta4'] = false;
+        validar['pregunta5'] = false;
     }
 })
 
 
 question6.addEventListener('blur',()=>{
-    const valor = document.getElementById('update_').value;
-    if(valor== `db.profesores.update({_id:1},{$set:{nombre:"lucas"}})` || valor== `db.profesores.update( {_id:1} , {$set:{nombre:"lucas"} } )`|| valor== `db.profesores.update( {_id:1} , { $set:{nombre:"lucas"} } )` || valor== `db.profesores.update({ _id:1 },{ $set:{ nombre:"lucas" }})`){
+    const valor = document.getElementById('ques_6').value;
+    if(valor== 2){
         question6.classList.remove('incorrecto');
         question6.classList.add('correcto');
-        validar['pregunta5'] = true; 
+        validar['pregunta6'] = true; 
     }else{
         question6.classList.remove('correcto');
         question6.classList.add('incorrecto');
-        validar['pregunta5'] = false; 
+        validar['pregunta6'] = false; 
     }
 })
  
-
-question7.addEventListener('blur',()=> {
-    const valor = document.getElementById('drop_3').value;
-    if(valor == 4){
-        question7.classList.remove('incorrecto');
-        question7.classList.add('correcto');
-        validar['pregunta6'] = true; 
-    }else{
-        question7.classList.remove('correcto');
-        question7.classList.add('incorrecto');
-        validar['pregunta6'] = false; 
-    }
-
-});
-
-
-question8.addEventListener('blur',()=> {
-    const valor = document.getElementById('drop_4').value;
-    if(valor == "db.dropDatabase()"){
-        question8.classList.remove('incorrecto');
-        question8.classList.add('correcto');
-        validar['pregunta7'] = true; 
-        console.log(validar)
-    }else{
-        question8.classList.remove('correcto');
-        question8.classList.add('incorrecto');
-        validar['pregunta7'] = false; 
-    }
-
-});
-
 if(validar.pregunta1== true && validar.pregunta2== true && validar.pregunta2a== true && validar.pregunta3== true && validar.pregunta4== true && validar.pregunta5== true && validar.pregunta6== true && validar.pregunta7== true  ){
     const seguir = document.getElementById('seguir');
     seguir.style.display="flex"
